2523c0ffef94eb12e6001c1b5d927aec53f70a10
hshub <hshub@hshub.io>
2022-10-04 22:02:48 +0000

    placeholder color changes

--- etc/session.php
+++ etc/session.php
@@ -7,7 +7,7 @@
 	$user = @$_SESSION["id"];
 	$userInfo = userInfo($user);
 
-	$revision = "20221004v5";
+	$revision = "20221004v6";
 
 	$self = @$_SERVER["PHP_SELF"]; 
 	$serverName = @$_SERVER["SERVER_NAME"]; 
