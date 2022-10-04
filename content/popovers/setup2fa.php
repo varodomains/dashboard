99f5888d7aee707267fc4696ff421516682554cd
hshub <hshub@hshub.io>
2022-10-04 19:04:50 +0000

    show 2fa secret

--- content/popovers/setup2fa.php
+++ content/popovers/setup2fa.php
@@ -7,8 +7,8 @@
 		<form id="setup2fa">
 			<div class="subtitle">Scan the QR code below and enter a code to complete Two-Factor Authentication setup.</div>
 			<div id="qrcode"></div>
+			<input type="text" name="code" readonly>
 			<input type="text" name="passcode" autocomplete="one-time-code" placeholder="123456" />
-			<input type="hidden" name="code">
 			<input type="hidden" name="action" value="setup2fa">
 			<div class="submit" data-action="setup2fa">Save</div>
 		</form>
