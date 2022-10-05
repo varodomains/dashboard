cf6962b80b960207cc23f646b0aad3c3ef0b485f
hshub <hshub@hshub.io>
2022-10-05 04:34:50 +0000

    bump version

--- etc/session.php
+++ etc/session.php
@@ -7,7 +7,7 @@
 	$user = @$_SESSION["id"];
 	$userInfo = userInfo($user);
 
-	$revision = "20221004v6";
+	$revision = "20221005v1";
 
 	$self = @$_SERVER["PHP_SELF"]; 
 	$serverName = @$_SERVER["SERVER_NAME"]; 
