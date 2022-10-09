8e39caa8a425e99a5b4298d4231aa6238b37c2de
hshub <hshub@hshub.io>
2022-10-09 17:01:16 +0000

    use button foreground for hns logo inside a submit button

--- etc/session.php
+++ etc/session.php
@@ -7,7 +7,7 @@
 	$user = @$_SESSION["id"];
 	$userInfo = userInfo($user);
 
-	$revision = "20221009v1";
+	$revision = "20221009v2";
 
 	$self = @$_SERVER["PHP_SELF"]; 
 	$serverName = @$_SERVER["SERVER_NAME"]; 
