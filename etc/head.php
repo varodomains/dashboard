d3b4516087de4faa497df3a1811c456956aeb71d
hshub <hshub@hshub.io>
2022-11-29 14:27:58 +0000

    add notranslate meta tag

--- etc/head.php
+++ etc/head.php
@@ -1,4 +1,5 @@
 <meta charset="utf-8">
+<meta name="google" content="notranslate"> 
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title data-name="<?php echo $GLOBALS["siteName"]; ?>"><?php echo $GLOBALS["siteName"]; ?></title>
 <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
