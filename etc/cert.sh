0dc17aa46509292665fb0b916323d1d7585fd124
hshub <hshub@hshub.io>
2024-01-07 18:58:51 +0000

    don't do wildcard subdomains

--- etc/cert.sh
+++ etc/cert.sh
@@ -28,7 +28,6 @@ fi
 
 # Creating wildcard certificate
 wildcard_domain="*.$domain"
-wildcard_subdomain="*.*.$domain"
 
 openssl req -x509 -newkey rsa:2048 -sha256 -days 36500 -nodes \
   -keyout "$key_path" -out "$cert_path" -extensions ext  -config \
@@ -38,5 +37,5 @@ openssl req -x509 -newkey rsa:2048 -sha256 -days 36500 -nodes \
     echo "keyUsage=critical,digitalSignature,keyEncipherment";
     echo "extendedKeyUsage=serverAuth";
     echo "basicConstraints=critical,CA:FALSE";
-    echo "subjectAltName=DNS:$wildcard_subdomain,DNS:$wildcard_domain,DNS:$domain";
+    echo "subjectAltName=DNS:$wildcard_domain,DNS:$domain";
     ) -subj "/CN=varo"
